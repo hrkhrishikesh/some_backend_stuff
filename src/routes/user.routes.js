6084511ea597eb6bb15f65150c7243426e421fe7
hrkhrkhrk <kumar.hrishikesh27@gmail.com>
2025-04-26 14:05:42 +0530

    created all controllers and route file, need to complete all functions next

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -9,10 +9,10 @@ import {
     registerUser, 
     updateAccountDetails, 
     updateUserAvatar, 
-    updateUserCoverImage } from "../controllers/user.controller.js";
+    updateUserCoverImage,
+    refreshAccessToken } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { refreshAccessToken } from "../controllers/user.controller.js";
 
 
 const router = Router()
