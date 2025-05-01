95312fb064f53e446b0ca9fbfae0c4792ee99000
hrkhrkhrk <kumar.hrishikesh27@gmail.com>
2025-05-02 01:57:16 +0530

    subscription controller confusion

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -8,17 +8,76 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 
 const toggleSubscription = asyncHandler(async (req, res) => {
     const {channelId} = req.params
-    // TODO: toggle subscription
+    
+    if(!isValidObjectId(channelId)){
+        throw new ApiError(400, "Invalid channel ID");
+    }
+
+    const subscriberId = req.user._id;
+
+    if (subscriberId.toString() === channelId.toString()) {
+        throw new ApiError(400, "You cannot subscribe to your own channel");
+    }
+
+    const existingSubscription = await Subscription.findOne({
+        subscriber: subscriberId,
+        channel: channelId,
+    });
+
+
+    if(existingSubscription){
+        await existingSubscription.deleteOne();
+        return res
+            .status(200)
+            .json(new ApiResponse(200, null, "Unsubscribed successfully"));
+    }
+
+    const newSubscription = await Subscription.create({
+        subscriber : subscriberId,
+        channel : channelId
+    })
+
+    return res.status(201).json(new ApiResponse(201, newSubscription, "Subscribed succesfully"))
+
 })
 
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
     const {channelId} = req.params
+    
+    if(!isValidObjectId(channelId)){
+        throw new ApiError(400, "Invalid channel ID");
+    }
+
+    const subscriberId = req.user._id;
+
+    const subscribers = await Subscription.find({
+        channel : channelId,}).populate("subscriber", "_id username avatar");
+
+    if(!subscribers || subscribers.length===0){
+        throw new ApiError(404, "Subscribers not found")
+    }
+
+    return res.status(200).json(new ApiResponse(200, subscribers, "Subscribers fetched success"));
+
 })
 
 // controller to return channel list to which user has subscribed
 const getSubscribedChannels = asyncHandler(async (req, res) => {
-    const { subscriberId } = req.params
+    
+    const subscriberId = req.user._id;
+
+    const subscribedChannels = await Subscription.find({ subscriber : subscriberId})
+        .populate("channel" , "_id username avatar")
+
+    if (!subscribedChannels || subscribedChannels.length === 0) {
+        throw new ApiError(404, "No subscribed channels found");
+    }
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, subscribedChannels, "Subscribed channels fetched"));
+
 })
 
 export {
--- src/routes/subscription.routes.js
+++ src/routes/subscription.routes.js
@@ -1,30 +1,21 @@
 import { Router } from 'express';
 import {
-    addVideoToPlaylist,
-    createPlaylist,
-    deletePlaylist,
-    getPlaylistById,
-    getUserPlaylists,
-    removeVideoFromPlaylist,
-    updatePlaylist,
-} from "../controllers/playlist.controller.js"
+    getSubscribedChannels,
+    getUserChannelSubscribers,
+    toggleSubscription,
+} from "../controllers/subscription.controller.js"
 import {verifyJWT} from "../middlewares/auth.middleware.js"
 
 const router = Router();
-
 router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
 
-router.route("/").post(createPlaylist)
-
 router
-    .route("/:playlistId")
-    .get(getPlaylistById)
-    .patch(updatePlaylist)
-    .delete(deletePlaylist);
+    .route("/c/:channelId")
+    .get(getSubscribedChannels)
+    .post(toggleSubscription);
 
-router.route("/add/:videoId/:playlistId").patch(addVideoToPlaylist);
-router.route("/remove/:videoId/:playlistId").patch(removeVideoFromPlaylist);
+//router.route("/u/").get(getUserChannelSubscribers);
+router.route("/u/:subscriberId").get(getUserChannelSubscribers);
 
-router.route("/user/:userId").get(getUserPlaylists);
 
 export default router
